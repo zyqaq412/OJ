68b302cbacd8d5faa26d4cf4fc9953478e5619e2
zxwyhzy <2791517764@qq.com>
2022-11-05 18:12:17 +0800

    输出练习

--- JAVA_idea/nowcoder/T2022/T11_4/ShuChuLianXi.java
+++ JAVA_idea/nowcoder/T2022/T11_4/ShuChuLianXi.java
@@ -17,81 +17,47 @@ public class ShuChuLianXi {
      * @Date: 2022/11/4 22:45
      * Math.pow 返回第一个参数的第二个参数次方
      */
-    static Scanner in = new Scanner(System.in);
-    static int t = 0;
-    static Long l, k, r;
-    static boolean flag = false;
 
-    public static void main(String[] args) {
-        t = in.nextInt();
-        while (t-- > 0) {
-            l = in.nextLong();
-            r = in.nextLong();
-            k = in.nextLong();
-            long m = 1;
-            flag = false;
-            if (l==0 && k==0){
-                flag = true;
-                System.out.print(0+" ");
-            }
-            if (l < 2 && r>0){//
-                flag = true;
-                System.out.print(1 +" ");
-            }
-            if (k > 1){
-                while (m>r ||m < Math.pow(2,63)){
-                    m *= k;
-                    if (m >= l && m<=r){
+        static Scanner in = new Scanner(System.in);
+        static int t = 0;
+        static Long l, k, r;
+        static boolean flag = false;
+
+        public static void main(String[] args) {
+            t = in.nextInt();
+            while (t-- > 0) {
+                l = in.nextLong();
+                r = in.nextLong();
+                k = in.nextLong();
+                long m = 1;
+                flag = false;
+                if (l==0 && k==0){
+                    flag = true;
+                    System.out.print(0+" ");
+                }
+                if (l < 2 && r>0){//
+                    flag = true;
+                    System.out.print(1 +" ");
+                }
+                if (k > 1){
+                    int i = 1;
+                    while (i < 64 && Math.pow(k, i) < Math.pow(2, 63)){
+                        m =m * k;
+                        if (m >= l && m<=r){
                             flag = true;
                             System.out.print(m+" ");
+                        }
+                        i++;
                     }
                 }
-
+                if (!flag) System.out.print("None.");
+                System.out.println();
             }
-            if (!flag) System.out.print("None.");
-            System.out.println();
         }
     }
-}
-/*
-public class Main {
-    static Scanner in = new Scanner(System.in);
-    static int t = 0;
-    static Long l, k, r;
-    static boolean flag = false;
 
-    public static void main(String[] args) {
-        t = in.nextInt();
-        while (t-- > 0) {
-            l = in.nextLong();
-            r = in.nextLong();
-            k = in.nextLong();
-            long m = 1;
-            flag = false;
-            if (l==0 && k==0){
-                flag = true;
-                System.out.print(0+" ");
-            }
-            if (l < 2 && r>0){//
-                flag = true;
-                System.out.print(1 +" ");
-            }
-            if (k > 1){
-                int i = 1;
-                while (i < 64 && Math.pow(k, i) < Math.pow(2, 63)){
-                    m =m * k;
-                    if (m >= l && m<=r){
-                        flag = true;
-                        System.out.print(m+" ");
-                    }
-                    i++;
-                }
-            }
-            if (!flag) System.out.print("None.");
-            System.out.println();
-        }
-    }
-}*/
+
+
 
 
 /*
