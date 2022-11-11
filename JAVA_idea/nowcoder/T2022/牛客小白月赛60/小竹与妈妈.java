b6677c2b9754612c393e78dfc667eb4178957428
zxwyhzy <2791517764@qq.com>
2022-11-11 19:59:46 +0800

    .

---
+++ JAVA_idea/nowcoder/T2022/牛客小白月赛60/小竹与妈妈.java
@@ -0,0 +1,19 @@
+package 牛客小白月赛60;
+
+import java.util.Scanner;
+
+/**
+ * @title: 小竹与妈妈
+ * @Author zxwyhzy
+ * @Date: 2022/11/11 19:11
+ * @Version 1.0
+ */
+public class 小竹与妈妈 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = sc.nextInt();
+        System.out.println((x-b)/a);
+    }
+}
