6084168db8c20e83d8f7f62a926148093e81a700
zxwyhzy <2791517764@qq.com>
2022-11-27 20:11:21 +0800

    旋转函数

--- JAVA_idea/leetcode/T2022/数组相关/数组的旋转/no_396_旋转函数.java
+++ JAVA_idea/leetcode/T2022/数组相关/数组的旋转/no_396_旋转函数.java
@@ -18,11 +18,29 @@ public class no_396_旋转函数 {
     }
 
 
+
+
+    /*
+    给定一个长度为 n 的整数数组 nums 。
+    假设 arrk 是数组 nums 顺时针旋转 k 个位置后的数组，我们定义 nums 的 旋转函数  F 为：
+    F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1]
+    返回 F(0), F(1), ..., F(n-1)中的最大值 。
+    生成的测试用例让答案符合 32 位 整数。
+     */
     public int maxRotateFunction2(int[] nums) {
+        // sum 数组和    curSum 数组系数乘积和  ans 最大数
         int sum = 0, curSum = 0, ans = 0;
-        for (int i = 0; i < nums.length; ++i){sum += nums[i]; curSum += i * nums[i];}
+        for (int i = 0; i < nums.length; ++i){
+            sum += nums[i];
+            curSum += i * nums[i];
+        }
         ans = curSum;
         for (int i = nums.length - 1; i > 0; --i) {
+            /*
+                每次变化后的 乘积和 数组最后一个数的系数变为0 其他的系数加1
+                乘积和 = 上一个乘积和 + 数组和 - ((n - 1) * arrk[n - 1]) - (数组和里面的arrk[n - 1])
+                      = 上一个乘积和 + 数组和 - n*arrk[n - 1]
+            */
             curSum += sum - nums.length * nums[i];
             ans = Math.max(ans, curSum);
         }
