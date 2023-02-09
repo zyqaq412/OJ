df248a43b3643e567e4ca8306fe7b60867ebfd17
zxwyhzy <2791517764@qq.com>
2023-02-09 15:33:29 +0800

    删除排序数组中的重复项,力扣第26题，重新做的

--- JAVA_idea/leetcode/T2022/双指针/同向双指针_滑动窗口/no_26_删除排序数组中的重复项.java
+++ JAVA_idea/leetcode/T2022/双指针/同向双指针_滑动窗口/no_26_删除排序数组中的重复项.java
@@ -7,8 +7,8 @@ package 双指针.同向双指针_滑动窗口;
  * @Version 1.0
  * 第二十六题：删除有序数组中的重复项
  */
-public class no_26 {
-    public int removeDuplicates(int[] nums) {
+public class no_26_删除排序数组中的重复项 {
+    public int removeDuplicates1(int[] nums) {
      /*   int t = 0;
         for (int i = 0; i < nums.length; i ++ ) {
             if (i == 0 || nums[i] != nums[i - 1]) nums[t ++ ] = nums[i];
@@ -26,8 +26,13 @@ public class no_26 {
 
         }
         return i+1;
+    }
+    public int removeDuplicates2(int[] nums) {
+        int j=0,i;
+        for(i=1;i<nums.length;i++){
 
-
-
+            if(nums[j] != nums[i]) nums[++j] = nums[i];
+        }
+        return j;
     }
 }
