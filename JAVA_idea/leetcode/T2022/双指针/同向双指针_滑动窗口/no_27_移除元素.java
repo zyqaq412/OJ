5ebed6ca9b20c91b3478c299cc46ed45f048172e
zxwyhzy <2791517764@qq.com>
2023-02-09 15:13:11 +0800

    移除元素,力扣第27题，重新做的

--- JAVA_idea/leetcode/T2022/双指针/同向双指针_滑动窗口/no_27_移除元素.java
+++ JAVA_idea/leetcode/T2022/双指针/同向双指针_滑动窗口/no_27_移除元素.java
@@ -7,8 +7,8 @@ package 双指针.同向双指针_滑动窗口;
  * @Version 1.0
  * 第二十七题：移除元素
  */
-public class no_27 {
-    public int removeElement(int[] nums, int val) {
+public class no_27_移除元素 {
+    public int removeElement1(int[] nums, int val) {
         int t = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != val){
@@ -17,5 +17,14 @@ public class no_27 {
             }
         }
         return t;
+    }
+    public int removeElement2(int[] nums, int val) {
+        int j = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
     }
 }
