0686f8f1f3f9633b8db5322d61f34c4fe12638a9
zxwyhzy <2791517764@qq.com>
2023-02-13 20:15:48 +0800

    力扣75题，颜色分类

---
+++ JAVA_idea/leetcode/T2022/数组相关/数组遍历/no_75_颜色分类.java
@@ -0,0 +1,40 @@
+package 数组相关.数组遍历;
+
+/**
+ * @title: no_75_颜色分类
+ * @Author zxwyhzy
+ * @Date: 2023/2/13 19:47
+ * @Version 1.0
+ *
+ * 给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ *
+ * 我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ * 必须在不使用库内置的 sort 函数的情况下解决这个问题。
+
+ */
+public class no_75_颜色分类 {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                ++p1;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                ++p0;
+                ++p1;
+            }
+        }
+    }
+}
