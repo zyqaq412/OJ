319971608ecca0a27d3a93c4818ee4fc5038c346
zxwyhzy <2791517764@qq.com>
2023-02-13 20:37:16 +0800

    力扣75题，颜色分类,另一种方法

--- JAVA_idea/leetcode/T2022/数组相关/数组遍历/no_75_颜色分类.java
+++ JAVA_idea/leetcode/T2022/数组相关/数组遍历/no_75_颜色分类.java
@@ -14,7 +14,7 @@ package 数组相关.数组遍历;
 
  */
 public class no_75_颜色分类 {
-    public void sortColors(int[] nums) {
+    public void sortColors1(int[] nums) {
         int n = nums.length;
         int p0 = 0, p1 = 0;
         for (int i = 0; i < n; ++i) {
@@ -34,6 +34,24 @@ public class no_75_颜色分类 {
                 }
                 ++p0;
                 ++p1;
+            }
+        }
+    }
+    public void sortColors2(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p2 = n - 1;
+        for (int i = 0; i <= p2; ++i) {
+            while (i <= p2 && nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = temp;
+                --p2;
+            }
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                ++p0;
             }
         }
     }
