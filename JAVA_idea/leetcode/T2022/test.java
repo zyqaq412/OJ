47fd90325e8db616eafa58a31729441b6724b93c
zxwyhzy <2791517764@qq.com>
2023-02-18 20:25:02 +0800

    力扣167题，两数之和 II - 输入有序数组

---
+++ JAVA_idea/leetcode/T2022/test.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @title: test
+ * @Author zxwyhzy
+ * @Date: 2022/11/23 15:32
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        HashMap<String,Integer> strs = new HashMap<>();
+        //strs.put("s",1);
+        boolean s = strs.containsKey("s");
+        System.out.println(s);
+    }
+    /*public boolean containsDuplicate(int[] nums) {
+        HashMap hashMap = new HashMap();
+        for (int i = 0 ;i< nums.length;i++){
+            Object put = hashMap.put(1, nums[i]);
+            if (put !=)
+        }
+
+    }*/
+}
