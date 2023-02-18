d19bf7d997ea4003479fcef8c8985dd0e29aa480
zxwyhzy <2791517764@qq.com>
2023-02-18 21:28:07 +0800

    力扣125题，验证回文串，更新了新的做法

--- JAVA_idea/leetcode/T2022/字符串/双指针/no_125_验证回文串.java
+++ JAVA_idea/leetcode/T2022/字符串/双指针/no_125_验证回文串.java
@@ -10,11 +10,13 @@ public class no_125_验证回文串 {
 
     public static void main(String[] args) {
         //String s = "A man, a plan, a canal: Panama";
-        String s = "`l;`` 1o1 ??;l`";
+        String s = "0p";
+        //String s = "`l;`` 1o1 ??;l`";
+
         //String s = "aaa,,,9090   adda";
         /*s = s.replaceAll("\\d|\\p{P}|\\s","");
         System.out.println(s);*/
-        System.out.println(new no_125_验证回文串().isPalindrome(s));
+        System.out.println(new no_125_验证回文串().isPalindrome2(s));
     }
 
 
@@ -45,4 +47,51 @@ public class no_125_验证回文串 {
         }
         return true;
     }
+    public boolean isPalindrome2(String s) {
+        // 65A---90Z     97a-----122z  0-48   9-57
+        //转换为小写
+        if (s.length()<1) return true;
+        s = s.toLowerCase();
+        int l=0,r=s.length()-1;
+
+        while (l<r){
+            //Character.isLetterOrDigit() 确定指定字符（Unicode 代码点）是字母还是数字。
+            //返回 true，则认为该字符是字母或数字   可以替换下面这个判断
+            while (!((s.charAt(l) >=97 && s.charAt(l)<=122)||
+                    (s.charAt(l)>=48&&s.charAt(l)<=57))) {
+                if (l>=r) return true;
+                l++;
+            }
+            while (!((s.charAt(r) >=97 && s.charAt(r)<=122)||
+                    (s.charAt(r)>=48&&s.charAt(r)<=57))) r--;
+            if (s.charAt(l++) == s.charAt(r--));
+            else return false;
+        }
+        return true;
+
+    }
+    /*
+    * 官方写法 和上面这个想法一致
+    * */
+    public boolean isPalindrome3(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+
 }
