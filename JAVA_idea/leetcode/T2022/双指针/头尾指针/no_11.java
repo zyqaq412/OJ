a012c5a0dc921ad0ee554c023f2ba0e77dd92da6
zxwyhzy <2791517764@qq.com>
2023-02-21 16:35:02 +0800

    力扣11题，盛最多水的容器

--- JAVA_idea/leetcode/T2022/双指针/头尾指针/no_11.java
+++ JAVA_idea/leetcode/T2022/双指针/头尾指针/no_11.java
@@ -6,9 +6,39 @@ package 双指针.头尾指针;
  * @Date: 2022/10/20 19:04
  * @Version 1.0
  * 第十一题：盛最多水的容器
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+
  */
 public class no_11 {
-    public int maxArea(int[] h) {
+    public int maxArea(int[] height) {
+        int l=0,r=height.length-1;
+        int area=Math.min(height[l],height[r])*(r-l);
+
+        while (l<r){
+            if (height[l] < height[r]){
+                l++;
+                area = Math.max(area,Math.min(height[l],height[r])*(r-l));
+            }else {
+                r--;
+                area = Math.max(area,Math.min(height[l],height[r])*(r-l));
+            }
+        }
+        return area;
+    }
+
+
+
+
+
+
+
+/*    public int maxArea(int[] h) {
         int max = 0;
         int i = 0;
         int j = h.length-1;
@@ -18,6 +48,5 @@ public class no_11 {
                     Math.max(max,(j-i)*h[i++]);
         }
         return max;
-    }
-
+    }*/
 }
