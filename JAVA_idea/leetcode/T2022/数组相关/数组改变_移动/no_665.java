4bb25fba7d170a375e03e873ee4dd215208c700c
zxwyhzy <2791517764@qq.com>
2023-03-01 23:08:19 +0800

    力扣665题，非递减数列,二刷

--- JAVA_idea/leetcode/T2022/数组相关/数组改变_移动/no_665.java
+++ JAVA_idea/leetcode/T2022/数组相关/数组改变_移动/no_665.java
@@ -16,6 +16,33 @@ import com.sun.xml.internal.ws.api.pipe.Tube;
 public class no_665 {
     //[3,4,2,3]
     //因为只能改应该数字所以 最多有 一个i不满足nums[i] <= nums[i + 1] 的时候有可能改为非递减数列
+
+
+    // 二刷
+    public boolean checkPossibility2(int[] nums) {
+        int n = nums.length, cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
     public boolean checkPossibility(int[] nums) {
         int n = nums.length;
         for (int i = 0; i < n - 1; ++i) {
