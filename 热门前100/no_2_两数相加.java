40c99968806c15c48484cec749e7a2696ea15f0d
zxwyhzy <2791517764@qq.com>
2023-04-15 22:42:59 +0800

    no_2_两数相加

--- 热门前100/no_2_两数相加.java
+++ 热门前100/no_2_两数相加.java
@@ -1,27 +1,56 @@
 public class no_2_两数相加{
+/* 
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 
+ */
 }
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ln = new ListNode(); 
+        // 第一次 进位为0
         add(l1, l2,0,ln);
         return ln.next;
         
     }
+    /**
+     * 
+     * @param l1 加数
+     * @param l2 加数
+     * @param n  进位
+     * @param ln 和
+     */
+    
     public void add(ListNode l1, ListNode l2,int n,ListNode ln){
+        // 两个链表都空 结束递归
         if(l1 == null && l2 == null) {
-            if(n==0) ln.next = new ListNode(n);
+            // 结束前判断上一次相加有没有进位  有就加上
+            if(n>0) ln.next = new ListNode(n);
             return;
         }
+        // 如果有一个链表空了就补零
         int n1 = l1==null?0:l1.val;
         int n2 = l2==null?0:l2.val;
         ln.next = new ListNode(n1+n2+n);
+        n = 0;
+        // 判断是否产生进位
         if(ln.next.val>=10){
+            n = ln.next.val/10;
             ln.next.val %= 10;
-            n = 1;
-        }
         
-        add(l1.next,l2.next,n,ln.next);
+        }
+        // 防止空指针异常
+        add(l1 == null? null:l1.next,
+            l2 == null? null:l2.next,
+            n,ln.next);
     }
 }
 class ListNode {
--- 热门前100/no_4_寻找两个正序数组的中位数.java
+++ 热门前100/no_4_寻找两个正序数组的中位数.java
@@ -1,23 +1,53 @@
 public class no_4_寻找两个正序数组的中位数 {
-    
+    /* 
+     * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+
+算法的时间复杂度应该为 O(log (m+n)) 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/median-of-two-sorted-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
 }
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len = nums1.length + nums2.length;
-
-        if(len &1 ==0){
-
-        }else{
-
+        // 记录两个数组长度
+        int l1 = nums1.length; int l2 =nums2.length; 
+        // 合并两个数组
+        double[] arr= new double[l1+l2];
+        // 遍历数组指针
+        int l = 0; int r = 0;
+        // true 数组长度是偶数  false 数组长度是奇数
+        boolean falg = ((l1+l2)&1) ==0 ? true:false;
+        for(int i=0;i<l1+l2;i++){
+            if(l<l1&&r<l2){
+                if(nums1[l] < nums2[r]){
+                    arr[i] = nums1[l];
+                    l++;
+                }else{
+                    arr[i] = nums2[r];
+                    r++;
+                }
+            }else{
+                if(l<l1){
+                    arr[i] = nums1[l];
+                    l++;
+                }else{
+                    arr[i] = nums2[r];
+                    r++;
+                }
+            }
+            // 求中位数合并的数组有一半就行了
+            if(i == (l1+l2)/2){
+                if(falg){
+                    return (arr[i]+arr[i-1])/2;
+               }else{
+                    return arr[i];
+               }
+            }
+            
         }
-
-
-
-
-
-
-
-        return 0 ;
+        return 0;
 
     }
 }
