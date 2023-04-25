ff6d47214f2c34589e499779e83bbe48f674f084
zxwyhzy <2791517764@qq.com>
2023-04-25 21:24:23 +0800

    力扣热门100 17.电话号码的字母组合 回溯法（深度优先搜索）

--- 热门前100/no_17_电话号码的字母组合.java
+++ 热门前100/no_17_电话号码的字母组合.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @title: no_17_电话号码的字母组合
@@ -14,25 +15,40 @@ public class no_17_电话号码的字母组合 {
     //TODO 以后做
 
     class Solution {
+        Map<Character, String> phoneMap = new HashMap<>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        List<String> res = new ArrayList<String>();
+        StringBuffer buffer = new StringBuffer();
         public List<String> letterCombinations(String digits) {
-            HashMap<Integer,String> hm = new HashMap<>();
-            hm.put(2,"abc");
-            hm.put(3,"def");
-            hm.put(4,"ghi");
-            hm.put(5,"jkl");
-            hm.put(6,"mno");
-            hm.put(7,"pqrs");
-            hm.put(8,"tuv");
-            hm.put(9,"wxyz");
-            List<String> res = new ArrayList<>();
-            int len = digits.length();
-            for (int i = 0; i < len; i++) {
+            // 长度为0直接返回空集
+            if (digits.length() == 0)  return res;
+            // 回溯法（深度优先搜索）
+            backtrack( digits, 0);
+            return res;
+        }
 
+        public void backtrack(String digits, int index) {
+            // 已经到最后一个结点
+            if (index == digits.length()) res.add(buffer.toString());
+            else {
+                // 获取当前按键
+                char digit = digits.charAt(index);
+                // 获取按键对应的字母映射
+                String letters = phoneMap.get(digit);
+                for (int i = 0; i < letters.length(); i++) {
+                    buffer.append(letters.charAt(i));
+                    backtrack( digits, index+1);
+                    buffer.deleteCharAt(index);
+                }
             }
-
-
-            return null;
-
         }
     }
 }
