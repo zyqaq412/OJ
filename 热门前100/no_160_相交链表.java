6979786c3ad0d8de874ff32cb3cb48798956e61b
zxwyhzy <2791517764@qq.com>
2023-05-08 13:52:33 +0800

    力扣热门100-160_相交链表 双指针加哈希表两种方法实现

--- 热门前100/no_160_相交链表.java
+++ 热门前100/no_160_相交链表.java
@@ -46,7 +46,11 @@ public class no_160_相交链表 {
             }
             return null;
         }
-
+        /*
+        * 如果两个链表相交，那么它们在相交节点之后的长度应该是相同的，
+        * 因此将两个指针分别从headA和headB开始移动，最终它们会在相交节点处相遇。
+        * 如果两个链表不相交，那么它们在结束时会同时到达链表的末尾，此时pA和pB均为null
+        * */
         public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
             ListNode pA = headA, pB = headB;
             while (pA != pB) {
