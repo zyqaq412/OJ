6c2f5beb36d84decf6a0c8b31f7d6476108fafe4
zyqaq412 <2791517764@qq.com>
2023-10-12 15:00:49 +0800

    no_27_移除元素

---
+++ oj_2023/src/双指针/no_27_移除元素.java
@@ -0,0 +1,28 @@
+package 双指针;
+
+/**
+ * @title: no_27_移除元素
+ * @Author zxwyhzy
+ * @Date: 2023/10/12 14:52
+ * @Version 1.0
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ */
+public class no_27_移除元素 {
+    public int removeElement(int[] nums, int val) {
+        int l = -1, r = 0;
+        while (r < nums.length) {
+            if (nums[r] == val) {
+                r++;
+            } else {
+                l++;
+                nums[l] = nums[r];
+                r++;
+            }
+        }
+        return l + 1;
+    }
+}
