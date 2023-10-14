cb8a44978b1c1c91fbfb85034fba517a1607c8a9
zyqaq412 <2791517764@qq.com>
2023-10-14 13:00:05 +0800

    no_102_二叉树的层序遍历 递归

---
+++ oj_2023/src/二叉树/no_102_二叉树的层序遍历.java
@@ -0,0 +1,27 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @title: no_102_二叉树的层序遍历
+ * @Author zxwyhzy
+ * @Date: 2023/10/14 12:50
+ * @Version 1.0
+ */
+public class no_102_二叉树的层序遍历 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        process(root,ans,0);
+        return ans;
+    }
+
+    // level 当前层数
+    public void process(TreeNode node, List<List<Integer>> ans, int level) {
+        if (node == null) return;
+        if (level >= ans.size()) ans.add(new ArrayList<>());
+        ans.get(level).add(node.val);
+        process(node.left,ans,level+1);
+        process(node.right,ans,level+1);
+    }
+}
