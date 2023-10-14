02b5be8b012252e0edce3110f5720533df95f45a
zyqaq412 <2791517764@qq.com>
2023-10-14 17:39:45 +0800

    no_515_在每个树行中找最大值 迭代

---
+++ oj_2023/src/二叉树/no_515_在每个树行中找最大值.java
@@ -0,0 +1,37 @@
+package 二叉树;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @title: no_515_在每个树行中找最大值
+ * @Author zxwyhzy
+ * @Date: 2023/10/14 17:37
+ * @Version 1.0
+ */
+public class no_515_在每个树行中找最大值 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode poll = queue.poll();
+                max = max > poll.val ? max : poll.val;
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
