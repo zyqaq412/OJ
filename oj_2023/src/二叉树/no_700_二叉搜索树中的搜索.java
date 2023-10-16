068d987eff28214dc0a2d31eff073fbc825504f9
zyqaq412 <2791517764@qq.com>
2023-10-16 15:13:25 +0800

    no_700_二叉搜索树中的搜索

---
+++ oj_2023/src/二叉树/no_700_二叉搜索树中的搜索.java
@@ -0,0 +1,19 @@
+package 二叉树;
+
+/**
+ * @title: no_700_二叉搜索树中的搜索
+ * @Author zxwyhzy
+ * @Date: 2023/10/16 15:07
+ * @Version 1.0
+ */
+public class no_700_二叉搜索树中的搜索 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
