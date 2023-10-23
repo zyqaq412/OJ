1f3db13d2d1c7fa8cb6966892b1069976a51fbb8
zyqaq412 <2791517764@qq.com>
2023-10-23 16:03:58 +0800

    no_131_分割回文串 回溯

---
+++ oj_2023/src/回溯/组合问题/no_131_分割回文串.java
@@ -0,0 +1,51 @@
+package 回溯.组合问题;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @title: no_131_分割回文串
+ * @Author zxwyhzy
+ * @Date: 2023/10/23 15:39
+ * @Version 1.0
+ */
+public class no_131_分割回文串 {
+    List<List<String>> lists = new ArrayList<>();
+    Deque<String> deque = new LinkedList<>();
+
+    public List<List<String>> partition(String s) {
+        backTracking(s, 0);
+        return lists;
+    }
+
+    private void backTracking(String s, int startIndex) {
+        //如果起始位置大于s的大小，说明找到了一组分割方案
+        if (startIndex >= s.length()) {
+            lists.add(new ArrayList(deque));
+            return;
+        }
+        for (int i = startIndex; i < s.length(); i++) {
+            //如果是回文子串，则记录
+            if (isPalindrome(s, startIndex, i)) {
+                String str = s.substring(startIndex, i + 1);
+                deque.addLast(str);
+            } else {
+                continue;
+            }
+            //起始位置后移，保证不重复
+            backTracking(s, i + 1);
+            deque.removeLast();
+        }
+    }
+    //判断是否是回文串
+    private boolean isPalindrome(String s, int startIndex, int end) {
+        for (int i = startIndex, j = end; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
